e3dec3b2ce9b9effe0a688484e3484bc3e942db7
BuildTools <amp7368@rit.edu>
2020-10-25 13:55:47 -0400

    edited the help message

--- src/main/java/apple/inactivity/discord/commands/general/CommandHelp.java
+++ src/main/java/apple/inactivity/discord/commands/general/CommandHelp.java
@@ -7,6 +7,7 @@ public class CommandHelp {
     public static void dealWithCommand(MessageReceivedEvent event) {
         event.getChannel().sendMessage("**" + Commands.INACTIVITY.getBareUsageMessage() + "** - gives an inactivity report for a guild. The guild name is case insensitive and a full guild name is not required\n" +
                 "**" + Commands.HELP.getBareUsageMessage() + "** - gives this help message\n" +
-                "**" + Commands.SUGGEST.getBareUsageMessage() + "** - sends an optional attachment as well as your message to appleptr16").queue();
+                "**" + Commands.SUGGEST.getBareUsageMessage() + "** - sends an optional attachment as well as your message to appleptr16\n" +
+                "https://discord.gg/XEyUWu9 is appleptr16#5054's discord server if you need to come over for some reason.").queue();
     }
 }
